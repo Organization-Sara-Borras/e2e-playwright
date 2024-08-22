e11d35bd7ee3f244a4fa2274de18f2a73cef73fb
Sara <sarabor09@gmail.com>
2024-08-22 11:04:16 +0200

    scenario

--- src/features/login.feature
+++ src/features/login.feature
@@ -4,4 +4,8 @@ Feature: Login SauceLabs
     Given the user is on sauceLabs login page
     When the user fill username with "standar_user"
     And the user fill password with "secret_sauce"
+    And the user press login button
+    And the user should see products page
+
+    
     
--- src/steps/login_steps.ts
+++ src/steps/login_steps.ts
@@ -1,10 +1,40 @@
 import { Given, When, Then} from '@cucumber/cucumber';
 
 When('I test playwright record', async function () {
-    await this.page.goto('https://www.saucedemo.com/');
-    await this.page.locator('[data-test="username"]').click();
-    await this.page.locator('[data-test="username"]').fill('standard_user');
-    await this.page.locator('[data-test="password"]').click();
-    await this.page.locator('[data-test="password"]').fill('secret_sauce');
-    await this.page.locator('[data-test="password"]').press('Enter');
-  });
+   
+    
+
+Given('the user is on sauceLabs login page', async function () => {
+
+    await this.page.goto(this.URL);
+})
+
+When('the user fill username with {string}', async function (username: string) {
+
+    await this.page.locator('[data-test="username"]').fill('username');
+   
+})
+
+When('the user fill password with {string}', async function (password: string) {
+  
+  await this.page.locator('[data-test="password"]').fill('password');
+})
+
+When('the user press login button', async function () {
+ 
+  await this.page.locator('[data-test="login-button"]').click();
+
+})
+
+Then('the user should see products page', async function () {
+
+  await expect(this.page.locator('[data-test="tittle"]')).toBeVisible();
+
+})
+
+
+
+
+
+
+
