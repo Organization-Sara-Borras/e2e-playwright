31d59ad526e6c05db8b0e35efd4b474fad2fd1ea
Sara <sarabor09@gmail.com>
2024-08-31 12:38:55 +0200

    fix login test

--- src/features/login.feature
+++ src/features/login.feature
@@ -2,10 +2,10 @@ Feature: Login SauceLabs
 
   Scenario: Login with valid username and password
     Given the user is on sauceLabs login page
-    When the user fill username with "standar_user"
+    When the user fill username with "standard_user"
     And the user fill password with "secret_sauce"
     And the user press login button
-    And the user should see products page
+    Then the user should see products page
 
     
     
--- src/steps/login_steps.ts
+++ src/steps/login_steps.ts
@@ -1,4 +1,5 @@
 import { Given, When, Then} from '@cucumber/cucumber';
+import { expect } from 'playwright/test';
 
 When('I test playwright record', async function () {
 });
@@ -12,13 +13,13 @@ Given('the user is on sauceLabs login page', async function () {
 
 When('the user fill username with {string}', async function (username: string) {
 
-    await this.page.locator('[data-test="username"]').fill('username');
+    await this.page.locator('[data-test="username"]').fill(username);
    
 })
 
 When('the user fill password with {string}', async function (password: string) {
   
-  await this.page.locator('[data-test="password"]').fill('password');
+  await this.page.locator('[data-test="password"]').fill(password);
 })
 
 When('the user press login button', async function () {
@@ -27,11 +28,11 @@ When('the user press login button', async function () {
 
 })
 
-// Then('the user should see products page', async function () {
+Then('the user should see products page', async function () {
 
-//   await expect(this.page.locator('[data-test="tittle"]')).toBeVisible();
+  await expect(this.page.locator('[data-test="title"]')).toBeVisible();
 
-// })
+})
 
 
 
